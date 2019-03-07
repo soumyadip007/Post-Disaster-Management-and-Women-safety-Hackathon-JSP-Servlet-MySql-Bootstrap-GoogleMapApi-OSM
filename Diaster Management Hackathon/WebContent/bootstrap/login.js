d6ff40b19d52d94e648e6e7770bc84c44d26240e
soumyadip007 <soumyadip.note@gmail.com>
2019-03-08 00:00:39 +0530

    Women safety form,Server

---
+++ Diaster Management Hackathon/WebContent/bootstrap/login.js
@@ -0,0 +1,51 @@
+	var i=0;
+	$(document).ready(function(){
+     $('#add_more').on('click', function(){
+      var colorR = Math.floor((Math.random() * 256));
+      var colorG = Math.floor((Math.random() * 256));
+      var colorB = Math.floor((Math.random() * 256));
+      i++;
+      var html ='<div id="append_no_'+i+'" class="animated bounceInLeft">'+
+          '<div class="input-group mt-3">'+
+		  '<div class="input-group-prepend">'+
+		  '<span class="input-group-text br-15" style="color:rgb('+colorR+','+colorG+','+colorB+'">'+
+		  '<i class="fas fa-user-graduate"></i></span>'+
+		  '</div>'+
+		  '<input type="text" placeholder="Student Name"  class="form-control"/>'+
+		  '</div>'+
+		  '<div class="input-group mt-3">'+
+		  '<div class="input-group-prepend">'+
+		  '<span class="input-group-text br-15" style="color:rgb('+colorR+','+colorG+','+colorB+'">'+
+		  '<i class="fas fa-phone-square"></i></span>'+
+		  '</div>'+
+		  '<input type="text" placeholder="Student Phone" class="form-control"/>'+
+		  '</div>'+
+		  '<div class="input-group mt-3">'+
+		  '<div class="input-group-prepend">'+
+		  '<span class="input-group-text br-15" style="color:rgb('+colorR+','+colorG+','+colorB+'">'+
+		  '<i class="fas fa-at"></i></span>'+
+		  '</div>'+
+		  '<input type="email" placeholder="Student Email" class="form-control"/>'+
+		  '</div></div>';
+
+	  $('#dynamic_container').append(html);
+	  $('#remove_more').fadeIn(function(){
+	  	 $(this).show();
+	  });
+     });
+
+     $('#remove_more').on('click', function(){
+         
+         $('#append_no_'+i).removeClass('bounceInLeft').addClass('bounceOutRight')
+            .fadeOut(function(){
+            	$(this).remove();
+            });
+            i--;
+            if(i==0){
+            	$('#remove_more').fadeOut(function(){
+            		$(this).hide()
+            	});;
+            }
+   
+     });
+	});
