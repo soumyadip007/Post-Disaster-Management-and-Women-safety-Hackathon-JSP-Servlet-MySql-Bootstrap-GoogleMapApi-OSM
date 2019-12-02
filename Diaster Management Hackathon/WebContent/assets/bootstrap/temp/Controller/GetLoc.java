94e645eca87ac8fb744d6c413bf54c24d183d6ce
unknown <soumyadip.note@GMAIL.COM>
2019-12-02 13:37:23 +0530

    Dest changed

--- Diaster Management Hackathon/WebContent/assets/bootstrap/temp/Controller/GetLoc.java
+++ Diaster Management Hackathon/WebContent/assets/bootstrap/temp/Controller/GetLoc.java
@@ -18,58 +18,46 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * 
  * @author Soumyadip Chowdhury
+ * @github soumyadip007
  *
  */
-@WebServlet("/addevents")
+@WebServlet("/GetLoc")
 public class GetLoc extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
 		String name=request.getParameter("name");
-		String start=request.getParameter("sdate");
-		String end=request.getParameter("edate");
-		String venue=request.getParameter("venue");
-		String des=request.getParameter("des");
-
-		String fee=request.getParameter("fee");
-		String link=request.getParameter("link");
+		String mob=request.getParameter("phone");
+		String type=request.getParameter("type");
+		String latt=request.getParameter("latt");
+		String longi=request.getParameter("longi");
+		String age=request.getParameter("age");
+		String blood=request.getParameter("blood");
 		try {
 		
+		ServletContext ct=getServletContext();
 		
 		Class.forName("com.mysql.jdbc.Driver");
-		
-		ServletContext ct=getServletContext();
 		
 		Connection con=(Connection) DriverManager.getConnection(ct.getInitParameter("path"),ct.getInitParameter("user"),ct.getInitParameter("pass"));
 		
 	//	Connection con=(Connection) DriverManager.getConnection("jdbc:mysql://node12654-getsaved.cloudjiffy.net/hackathon","root","THHrio35634");
 		
-		PreparedStatement st=con.prepareStatement("INSERT INTO event(name,start,end,venue,des,fee,link) values(?,?,?,?,?,?,?)");
+		PreparedStatement st=con.prepareStatement("INSERT INTO hack(name,ph,type,latt,longi,age,blood) values(?,?,?,?,?,?,?)");
+		
 		
 			st.setString(1,name);
-			st.setString(2,start);
-			st.setString(3,end);
-			st.setString(4,venue);
-			st.setString(5,des);
-
-			st.setString(6,fee);
-			st.setString(7,link);
-		
+			st.setString(2,mob);
+			st.setString(3,type);
+			st.setString(4,latt);
+			st.setString(5,longi);
+			st.setString(6,age);
+			st.setString(7,blood);
 			int i=st.executeUpdate();
-			
-			st=con.prepareStatement("INSERT INTO allevents(name,start,end,venue) values(?,?,?,?)");
-			
-			st.setString(1,name);
-			st.setString(2,start);
-			st.setString(3,end);
-			st.setString(4,venue);
-			
-		
-			st.executeUpdate();
 		
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -84,11 +72,13 @@ public class GetLoc extends HttpServlet {
 		
 		
 		
+		System.out.println("Latti"+latt);
+		System.out.println(longi);
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		//response.sendRedirect("https://www.latlong.net/c/?lat="+latt+"&long="+longi);
 	//	https://www.google.co.in/maps/@22.5601086,88.4904909,21z
 
-		response.sendRedirect("adminhome.jsp");
+		response.sendRedirect("success.html");
 	}
 
 
